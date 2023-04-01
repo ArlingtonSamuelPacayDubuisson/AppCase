08773903337fda8ceb9b4348393b2db89b14c4ab
Arlington <apacayd@miumg.edu.gt>
2023-04-01 09:34:22 -0600

    :rocket: Nueva app case

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Variables
+int option;
+double A;
+double B;
+double resultado;
+string mensaje;
+
+//Dibujar menu
+console.WriteLine("--CALCULADORA--");
+console.WriteLine("1. Sumar.");
+console.WriteLine("2. Restar.");
+console.WriteLine("3. Multiplicar.");
+console.WriteLine("4. Dividir.");
+console.WriteLine("5. Elevar a la potencia.");
+console.WriteLine("Selecccione una opcion (1 - 5): ");
+
+
+
